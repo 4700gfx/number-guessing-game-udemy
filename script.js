58d66caba1e911baf190e3d4e02ea4955dc7b11a
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-04-27 17:09:59 -0400

    Adding Check Button Logic

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//Variables for Elements 
+
+const message = document.querySelector('.message');
+const number = document.querySelector('.number');
+const score = document.querySelector('.score');
+let guessedNumber = document.querySelector('.guess');
+
+const checkButton = document.querySelector('.check')
+
+
+
+checkButton.addEventListener("click", ()=> {
+  const guess = guessedNumber.value;
+  console.log(guess)
+
+  if(!guess){
+    message.textContent = 'Not A Number ⛔️'
+  }
+
+
+})
