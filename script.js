29d7a63f65d6408d63f06cd0713dd58a2868c508
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-04-29 21:16:35 -0400

    Adding More Functionality to Check Button

--- script.js
+++ script.js
@@ -4,12 +4,16 @@
 
 const message = document.querySelector('.message');
 const number = document.querySelector('.number');
-const score = document.querySelector('.score');
 let guessedNumber = document.querySelector('.guess');
 
 const checkButton = document.querySelector('.check')
+const scoreDisplay = document.querySelector('.score');
+let score = 20;
 
 
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+number.textContent = secretNumber;
 
 checkButton.addEventListener("click", ()=> {
   const guess = guessedNumber.value;
@@ -17,7 +21,36 @@ checkButton.addEventListener("click", ()=> {
 
   if(!guess){
     message.textContent = 'Not A Number ⛔️'
-  }
+  } else if (guess == secretNumber){
+    message.textContent = 'Correct Number 🎉'
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    number.style.width = "30rem"
+    guessedNumber.value = " "
 
+  } else if (guess > secretNumber) {
+    if (score > 1 ){
+      message.textContent = 'Number Too High ⬆️'
+      score --;
+      scoreDisplay.textContent = score; 
+      guessedNumber.value = " "
+    } else {
+      message.textContent = 'YOU LOSE ‼️'
+      score = 0;
+      scoreDisplay.textContent = score; 
+      guessedNumber.value = " "
+    }
+  } else if (guess < secretNumber) {  
+    if (score > 1 ){
+      message.textContent = 'Number Too Low ⬆️'
+      score --;
+      scoreDisplay.textContent = score; 
+      guessedNumber.value = " "
+    } else {
+      message.textContent = 'YOU LOSE ‼️'
+      score = 0;
+      scoreDisplay.textContent = score; 
+      guessedNumber.value = " "
+    }
+  }
 
 })
